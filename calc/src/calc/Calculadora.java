fea6b21fa42d0921b329785e14e65a505fe32d8b
Bruno <brunomatrix1986@hotmail.com>
2016-05-10 21:09:10 -0300

    Calculadora

---
+++ calc/src/calc/Calculadora.java
@@ -0,0 +1,47 @@
+package calc;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	
+	public static void main(String[] args) {
+		
+		Calculadora c = new Calculadora();
+		
+		double resultado = c.Somar(8,8);
+		System.out.println(resultado);
+		
+		double resultado1 = c.Subtrair(5,3);
+		System.out.println(resultado1);
+		
+		double resultado2 = c.Multiplicar(5,3);
+		System.out.println(resultado2);
+		
+		double resultado3 = c.Dividir(6,2);
+		System.out.println(resultado3);
+			
+	}
+	
+	public double Somar(double Param1,double Param2){
+	
+	return Param1 + Param2;
+	
+	}
+	public double Subtrair(double Param1,double Param2){
+		
+		return Param1 - Param2;
+		
+		}
+	public double Multiplicar(double Param1,double Param2){
+		
+		return Param1 * Param2;
+		
+		}
+	public double Dividir(double Param1,double Param2){
+		
+		return Param1 / Param2;
+		
+		}
+	
+}
